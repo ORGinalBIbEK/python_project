855f134dbe3556ef3c4bbf030d62a2a382d161f8
Bibek <bibekmhrzn23@gmail.com>
2026-05-24 22:00:32 +0545

    tkinter

--- calculator.py
+++ calculator.py
@@ -11,6 +11,9 @@ button_value=[
 right_symbol=["/","X","-","+","="]
 top_symbol=["AC","DEL","%"]
 
+row_count=len(button_value) #5
+column_count=len(button_value[0]) #4
+
 #Background:
 color_dark_charcoal="#1C1C1E"
 
@@ -22,3 +25,16 @@ color_bright_color ="#FF9F0A"
 
 #Clear/Modifier Keys (AC, +/-, %):
 color_slate_gray="#A5A5A5"
+
+
+#window setup
+window=tkinter.Tk()
+window.title("Calculator")
+window.resizable(False,False)
+
+frame=tkinter.Frame(window,bg=color_dark_charcoal)
+label=tkinter.Label(frame,text="0",bg=color_dark_charcoal,fg="white",font=("Arial",30),anchor="e")
+
+label.grid(fill="both",expand=True,row=0,column=0)
+frame.pack(fill="both",expand=True)
+window.mainloop()
