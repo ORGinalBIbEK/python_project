add33b391c22ffba5e49bd4ba0ea3ca368eaada4
Bibek <bibekmhrzn23@gmail.com>
2026-05-24 21:49:08 +0545

    choosing color

---
+++ calculator.py
@@ -0,0 +1,24 @@
+import tkinter
+
+
+button_value=[
+    ["AC","DEL","%","/"],
+    ["7","8","9","X"],
+    ["4","5","6","-"],
+    ["1","2","3","+"],
+    ["0",".","+/-","="]
+]
+right_symbol=["/","X","-","+","="]
+top_symbol=["AC","DEL","%"]
+
+#Background:
+color_dark_charcoal="#1C1C1E"
+
+#Number Keys:
+color_light_greay="#3A3A3C"
+
+#Function Keys (+, −, ×, ÷):
+color_bright_color ="#FF9F0A"
+
+#Clear/Modifier Keys (AC, +/-, %):
+color_slate_gray="#A5A5A5"
